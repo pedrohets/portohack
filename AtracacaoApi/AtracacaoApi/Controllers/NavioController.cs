868c61d19531cef8927e65f20a5d579c064c178f
pedrohets <pedrohets@hotmail.com>
2022-07-31 01:47:05 -0300

    Criando controller de NAvio

---
+++ AtracacaoApi/AtracacaoApi/Controllers/NavioController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AtracacaoApi.Controllers
+{
+    /// <summary>
+    /// Informações dos navios
+    /// </summary>
+    [ApiController]
+    [Route("Navio")]
+    public class NavioController : ControllerBase
+    {
+        /// <summary>
+        /// Método para obter os dados do navio
+        /// </summary>
+        /// <returns>Dados do navio</returns>
+        [HttpGet("DadosNavio")]
+        public IActionResult ObterDadosNavio()
+        {
+            var retorno = new
+            {
+                Nome = "CAP SAN LORENZO",
+                Imo = "9622227",
+                Bandeira = "Denmark [DK]",
+                TipoNavio = "Container Ship"
+            };
+            return Ok(retorno);
+        }
+    }
+}
